79dd9033f668845f77068d026b9490b1ab7664b7
verifit <verifit@fit.vutbr.cz>
2023-07-03 12:22:45 +0200

    Fix minor issues in extract_benchmarks.sh

--- extract_benchmarks.sh
+++ extract_benchmarks.sh
@@ -4,7 +4,7 @@
 input_directory="benchmarks"
 
 # Create a directory to store the extracted contents
-output_directory="."
+output_directory=$(pwd)
 
 # Change to the input directory
 cd "$input_directory"
@@ -13,8 +13,6 @@ cd "$input_directory"
 for file in *.tgz.aa; do
     echo "extracting pack files: $file"
     # Create a directory for each file
-    output_subdirectory="$output_directory/${file%.tgz.aa}"
-    mkdir -p "$output_subdirectory"
     target=${file/.aa/}
     cat "$target".* > "$target"
 done
@@ -22,7 +20,14 @@ done
 for file in *.tgz; do
     echo "extracting single file: $file"
     # Create a directory for each file
-    output_subdirectory="$output_directory/${file%.tgz}"
+    # Note, we remove the one layer of directory for multipacks
+    # This way, we do not e.g. get `bool_comb/bool_comb` directory.
+    # It is hack, but what the heck.
+    if [ -f "$file.aa" ]; then
+        output_subdirectory="$output_directory"
+    else
+        output_subdirectory="$output_directory/${file%.tgz}"
+    fi
     mkdir -p "$output_subdirectory"
 
     # Extract the file into the corresponding directory
